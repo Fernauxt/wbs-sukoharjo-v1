928a74a6652ac5914d56c64d114bf0081fe5b3e8
Fernauxt <210711254@students.uajy.ac.id>
2025-03-14 11:15:59 +0700

    add how-to

--- resources/views/sections/hero.blade.php
+++ resources/views/sections/hero.blade.php
@@ -1,16 +1,20 @@
 <div class="hero min-h-screen bg-gradient-to-b from-base-900 to-base-100">
     <div class="hero-content flex flex-col items-center text-center">
         <div class="max-w-9xl" data-aos="fade-up" data-aos-duration="1000">
-            <h1 class="text-1xl font-base text-gray-900 lg:text-5xl lg:pt-10" data-aos="fade-down" data-aos-delay="100" data-aos-duration="1000">
+            <h1 class="text-1xl font-base text-gray-900 lg:text-5xl lg:pt-10" data-aos="fade-down" data-aos-delay="100"
+                data-aos-duration="1000">
                 Selamat Datang Di
             </h1>
-            <h1 class="text-2xl font-bold uppercase text-gray-900 pt-4 lg:text-6xl" data-aos="zoom-in" data-aos-delay="200" data-aos-duration="1000">
+            <h1 class="text-2xl font-bold uppercase text-gray-900 pt-4 lg:text-6xl" data-aos="zoom-in"
+                data-aos-delay="200" data-aos-duration="1000">
                 Whistleblowing System <br> Pemerintah Kabupaten Sukoharjo
             </h1>
-            <p class="pt-7 text-base text-gray-900 lg:text-xl" data-aos="fade-in" data-aos-delay="300" data-aos-duration="1000">
+            <p class="pt-7 text-base text-gray-900 lg:text-xl" data-aos="fade-in" data-aos-delay="300"
+                data-aos-duration="1000">
                 Dukung transparansi untuk pemerintahan yang bersih dan terpercaya.
             </p>
-            <p class="text-sm pt-1 text-gray-900 lg:text-lg" data-aos="fade-in" data-aos-delay="400" data-aos-duration="1000">
+            <p class="text-sm pt-1 text-gray-900 lg:text-lg" data-aos="fade-in" data-aos-delay="400"
+                data-aos-duration="1000">
                 Identitas Anda aman dan dirahasiakan. Kami menjamin laporan Anda diproses dengan transparan dan adil.
             </p>
             <button
@@ -19,15 +23,16 @@
             </button>
         </div>
         <div class="" data-aos="fade-up" data-aos-delay="500" data-aos-duration="1000">
-            <img src="{{ asset('img/img-hero.png') }}" alt="Ilustrasi Input Laporan" class="h-105" data-aos="fade" data-aos-delay="600" data-aos-duration="1000"/>
+            <img src="{{ asset('img/img-hero.png') }}" alt="Ilustrasi Input Laporan" class="h-105" data-aos="fade"
+                data-aos-delay="600" data-aos-duration="1000" />
         </div>
     </div>
 </div>
 
 <div class="hero min-h-screen bg-base-100">
     <div class="hero-content flex-col lg:flex-row-reverse lg:text-justify items-center text-center">
-        <img src="https://wbs.sukoharjokab.go.id/images/wb.png"
-            class="max-w-xs lg:max-w-md rounded-lg" data-aos="flip-left" data-aos-delay="200" data-aos-duration="1000" />
+        <img src="https://wbs.sukoharjokab.go.id/images/wb.png" class="max-w-xs lg:max-w-md rounded-lg"
+            data-aos="flip-left" data-aos-delay="200" data-aos-duration="1000" />
         <div data-aos="fade-right" data-aos-delay="300" data-aos-duration="1000">
             <h1 class="text-5xl lg:text-7xl font-semibold font-cursive text-red-600">Definisi WBS?</h1>
             <h3 class="text-3xl lg:text-5xl font-bold italic pt-1 lg:pt-5">Whistle Blowing System</h3>
@@ -55,7 +60,85 @@
 </div>
 
 <div class="hero min-h-screen bg-base-900 text-center">
-    <h1 class="text-2xl font-bold text-gray-900 pt-4 lg:text-6xl" data-aos="zoom-in" data-aos-delay="200" data-aos-duration="1000">
-        Kriteria Pengaduan
-    </h1>
+    <div class="hero-content flex-col">
+        <h1 class="text-2xl font-bold font-cursive text-red-600 pt-4 lg:text-6xl" data-aos="zoom-in"
+            data-aos-delay="200" data-aos-duration="1000">
+            Kriteria Pengaduan
+        </h1>
+        <p class="lg:pt-2 lg:text-2xl" data-aos="zoom-in" data-aos-delay="300" data-aos-duration="1500">Anda dapat
+            melaporkan pelanggaran atau dugaan pelanggaran yang dilakukan oleh
+            atasan atau rekan kerja dengan menjadi Whistleblower. Pengaduan Anda akan ditindaklanjuti jika memenuhi
+            kriteria berikut.</p>
+        <div data-aos="fade-up" data-aos-delay="400" data-aos-duration="2000">
+            <div class="grid grid-cols-1 md:grid-cols-3 gap-8 mt-8">
+                <div
+                    class="card-body bg-base-100 border-1 py-8 text-left rounded-2xl hover:border-0 hover:scale-110 hover:shadow-2xl transition-all duration-150 ease-in-out">
+                    <h2 class="text-lg font-semibold">Jenis Dugaan Pelanggaran <br> yang Dapat Diadukan</h2>
+                    <ul class="list-none mt-3 space-y-4">
+                        <li class="flex items-center mt-1.5">
+                            <img src="https://cdn-icons-png.flaticon.com/128/8265/8265301.png" alt="ikon1" class="w-6 h-6 mr-3" />
+                            <p class="text-lg mr-2">Korupsi</p>
+                        </li>
+                        <li class="flex items-center mt-1.5">
+                            <img src="https://cdn-icons-png.flaticon.com/128/8265/8265301.png" alt="ikon2" class="w-6 h-6 mr-3" />
+                            <p class="text-lg mr-2">Tindak Pidana Umum</p>
+                        </li>
+                        <li class="flex items-center mt-1.5">
+                            <img src="https://cdn-icons-png.flaticon.com/128/8265/8265301.png" alt="ikon3" class="w-6 h-6 mr-3" />
+                            <p class="text-lg mr-2">Pelecehan Seksual</p>
+                        </li>
+                        <li class="flex items-center mt-1.5">
+                            <img src="https://cdn-icons-png.flaticon.com/128/8265/8265301.png" alt="ikon4" class="w-6 h-6 mr-3" />
+                            <p class="text-lg mr-2">Pelanggaran Kode Etik ASN</p>
+                        </li>
+                        <li class="flex items-center mt-1.5">
+                            <img src="https://cdn-icons-png.flaticon.com/128/8265/8265301.png" alt="ikon5" class="w-6 h-6 mr-3" />
+                            <p class="text-lg mr-2">Kedisiplinan Pegawai</p>
+                        </li>
+                        <li class="flex items-center mt-1.5">
+                            <img src="https://cdn-icons-png.flaticon.com/128/8265/8265301.png" alt="ikon5" class="w-6 h-6 mr-3" />
+                            <p class="text-lg mr-2">Jenis Pelanggaran Pegawai Lainnya</p>
+                        </li>
+                    </ul>
+                </div>
+                <div
+                    class="card-body bg-base-100 border-1 py-8 text-left rounded-2xl hover:border-0 hover:scale-110 hover:shadow-2xl transition-all duration-150 ease-in-out">
+                    <h2 class="text-lg font-semibold">Kriteria Pengaduan <br> Indikasi Pelanggaran Pegawai</h2>
+                    {{-- <p class="mt-2 text-lg">Indikasi pelanggaran yang dapat diadukan adalah sebagai berikut:</p> --}}
+                    <ul class="list-none mt-3 space-y-4">
+                        <li class="flex items-center mt-1.5">
+                            <img src="https://cdn-icons-png.flaticon.com/128/2014/2014825.png" alt="ikon1" class="w-10 mr-3" />
+                            <p class="text-lg mr-2">Dugaan Pelanggaran Apa yang Dilakukan</p>
+                        </li>
+                        <li class="flex items-center mt-1.5">
+                            <img src="https://cdn-icons-png.flaticon.com/128/535/535137.png" alt="ikon2" class="w-10 mr-3" />
+                            <p class="text-lg mr-2">Dimana Dugaan Pelanggaran Terjadi</p>
+                        </li>
+                        <li class="flex items-center mt-1.5">
+                            <img src="https://cdn-icons-png.flaticon.com/128/1642/1642266.png" alt="ikon3" class="w-10 mr-3" />
+                            <p class="text-lg mr-2">Kapan Dugaan Pelanggaran Terjadi</p>
+                        </li>
+                        <li class="flex items-center mt-1.5">
+                            <img src="https://cdn-icons-png.flaticon.com/128/16864/16864683.png" alt="ikon4" class="w-10 mr-3" />
+                            <p class="text-lg mr-2">Siapa Pelaku Dugaan Pelanggaran</p>
+                        </li>
+                        <li class="flex items-center mt-1.5">
+                            <img src="https://cdn-icons-png.flaticon.com/128/13696/13696855.png" alt="ikon5" class="w-10 mr-3" />
+                            <p class="text-lg mr-2">Bagaimana Dugaan Pelanggaran Berlangsung</p>
+                        </li>
+                    </ul>
+                </div>
+                <div class="card-body bg-base-100 border-1 p-8 text-justify rounded-2xl hover:border-0 hover:scale-110 hover:shadow-2xl transition-all duration-150 ease-in-out">
+                    <figure class="px-10 pt-10">
+                        <img src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
+                            alt="Shoes" class="rounded-xl" />
+                    </figure>
+                    <div class="card-body items-center text-center">
+                        <h2 class="text-3xl font-semibold">What, Where, When, Who, Why, How?</h2>
+                        <p class="text-lg">Penjelasan pengaduan dugaan pelanggaran akan ditindaklanjuti jika memenuhi kriteria tersebut.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
---
+++ resources/views/sections/how-to.blade.php
@@ -0,0 +1,47 @@
+<div class="hero min-h-screen bg-base-900 text-center" id="cara-pengaduan">
+    <div class="hero-content flex-col">
+        <h1 class="text-2xl font-bold font-cursive text-red-600 pt-4 lg:text-6xl" data-aos="zoom-in" data-aos-delay="200"
+            data-aos-duration="1000">
+            Tata Cara Pengaduan
+        </h1>
+        <ul class="steps steps-vertical lg:steps-horizontal mt-6 lg:w-full justify-between" data-aos="zoom-out"
+            data-aos-delay="500" data-aos-duration="1500">
+            <li class="step step-error flex-2 my-2 text-center">
+                <div data-aos="fade-up" data-aos-delay="1000">
+                    <span class="font-bold text-lg block my-2">1. Buat Pengaduan</span>
+                    <p>Klik Tombol <a class="underline decoration-1 decoration-red-500 font-bold">Buat Pengaduan</a>
+                        untuk memulai proses pengaduan. Pengguna akan dialihkan ke laman formulir</p>
+                </div>
+            </li>
+            <li class="step step-error flex-2 my-2 text-center">
+                <div data-aos="fade-up" data-aos-delay="1250">
+                    <span class="font-bold text-lg block my-2">2. Pahami Kriteria</span>
+                    <p>Pastikan informasi dijelaskan dengan memenuhi unsur 5W+1H (Apa, Siapa, Kapan, Dimana, Mengapa,
+                        Bagaimana).</p>
+                </div>
+            </li>
+            <li class="step step-error flex-2 my-2 text-center">
+                <div data-aos="fade-up" data-aos-delay="1500">
+                    <span class="font-bold text-lg block my-2">3. Isi Formulir Pengaduan</span>
+                    <p>Isikan formulir yang sudah disediakan sesuai dengan data yang sesuai. Pastikan semua isian form
+                        terisi.</p>
+                </div>
+            </li>
+            <li class="step step-error flex-2 my-2 text-center">
+                <div data-aos="fade-up" data-aos-delay="1750">
+                    <span class="font-bold text-lg block my-2">4. Unggah Bukti</span>
+                    <p>Lampirkan bukti pelanggaran dalam bentuk file seperti foto atau dokumen lain dengan cara tambah
+                        file kemudian upload.</p>
+                </div>
+            </li>
+            <li class="step step-error flex-2 my-2 text-center">
+                <div data-aos="fade-up" data-aos-delay="2000">
+                    <span class="font-bold text-lg block my-2">5. Kirim Aduan</span>
+                    <p>Setelah formulir terisi, tekan tombol <a
+                            class="underline decoration-1 decoration-red-500 font-bold">Kirim Pengaduan</a>. Kode
+                        laporan akan dikirim guna memantau proses tindak lanjut.</p>
+                </div>
+            </li>
+        </ul>
+    </div>
+</div>
