ae407b9762af73012e40bd45433cc4bde03727ec
mieke <mikewahyu.189@gmail.com>
2026-02-05 09:28:45 +0700

    fix: add cloudinary config and on AppSerivceProvider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\URL;
+use Illuminate\Support\Facades\Config;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -22,6 +23,12 @@ class AppServiceProvider extends ServiceProvider
     {
         if (config('app.env') === 'production') {
             URL::forceScheme('https');
+        }
+
+        if (env('CLOUDINARY_URL')) {
+            Config::set('cloudinary.cloud_url', env('CLOUDINARY_URL'));
+            Config::set('cloudinary.upload_preset', env('CLOUDINARY_UPLOAD_PRESET'));
+            Config::set('cloudinary.notification_url', env('CLOUDINARY_NOTIFICATION_URL'));
         }
     }
 }
